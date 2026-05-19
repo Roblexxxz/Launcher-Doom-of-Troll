6ad8e3d5ffaa94267fd8b4fda95c40c6f4ea50b1
Roblexxxz <daniela4354@mytusd.org>
2026-05-19 17:15:41 +0000

    jgfhgvnhgngng

--- launcher.c
+++ launcher.c
@@ -1,10 +1,27 @@
-launcher.js.importmediafromsource
-  {Doom of troll}
-Media.c
-  fish.2.0
-  .interval = findvariable
-       Export { launcher }
-  xp.3.34.34.5/newfish
-      launch.newmedia import @ {Doom of troll}
-launch.newfunction.new
-  2.0/fish.o
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(int argc, char *argv[]) {
+    // Launcher for Doom-of-Troll (DuneDash)
+    
+    pid_t pid = fork();
+    
+    if (pid == 0) {
+        // Child process - execute DuneDash from submodule
+        chdir("Doom-of-Troll");
+        execvp("./Doom-of-Troll", argv);
+        perror("execvp failed");
+        exit(1);
+    } else if (pid > 0) {
+        // Parent process - wait for child
+        int status;
+        waitpid(pid, &status, 0);
+        return WEXITSTATUS(status);
+    } else {
+        perror("fork failed");
+        return 1;
+    }
+}
