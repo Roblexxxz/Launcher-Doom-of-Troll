ad3cc0082801b3809cbb83cc99130dbf4b456282
Roblexxxz <daniela4354@mytusd.org>
2026-05-19 09:36:44 -0700

    Create launcher.cmake

---
+++ launcher.cmake
@@ -0,0 +1,7 @@
+[main] 
+  import.troll.newinterval
+  set"troll.variable.new
+  launcher.connect:content.repo@Doom of troll
+    launcher.troll/loading = fish.c 
+    [loading]:service = newserver.new.connect
+    load.connect:repo.{main}
