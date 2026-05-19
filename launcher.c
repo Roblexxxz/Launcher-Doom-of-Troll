99ab0fb56258602ff4f5e22667c16c75e101fc60
Roblexxxz <daniela4354@mytusd.org>
2026-05-19 09:29:06 -0700

    Create launcher.c

---
+++ launcher.c
@@ -0,0 +1,10 @@
+launcher.js.importmediafromsource
+  {Doom of troll}
+Media.c
+  fish.2.0
+  .interval = findvariable
+       Export { launcher }
+  xp.3.34.34.5/newfish
+      launch.newmedia import @ {Doom of troll}
+launch.newfunction.new
+  2.0/fish.o
