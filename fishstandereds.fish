693ee41486ca06e1aea37dc47971aa6580faacf8
Roblexxxz <daniela4354@mytusd.org>
2026-05-20 09:06:59 -0700

    Create fishstandereds.fish

---
+++ fishstandereds.fish
@@ -0,0 +1,14 @@
+[main]
+fish.fish"NULL/0
+fish = 1.0 
+  [main]:
+     {trollLauncher}:fish
+     fish.convertpackage{JSON}
+     [main]:"fish"
+     troll."fish"
+     /github/deploy/import.c
+     {json{PARSE}
+       fish.JSON:newvariable.{import}
+  fish.c = value.launcher{main}
+     import = launcher.data
+     {main}.fish
