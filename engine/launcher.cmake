f801eae513ba1d5203998f60dda24f95a10d3136
Roblexxxz <daniela4354@mytusd.org>
2026-05-21 11:22:06 -0700

    Create launcher.cmake

---
+++ engine/launcher.cmake
@@ -0,0 +1,7 @@
+{main.cmake"}
+{troll.engine = standereds.json = convertpackage.json
+ {launcher.engine: threestandereds.troll
+{launcher:engine.loading:cplusplus.standereds:
+troll.new = c.fish
+fish.c = cstandereds.launcher.new 
+doom.fish = c standers.launcher.u.s.o.n
